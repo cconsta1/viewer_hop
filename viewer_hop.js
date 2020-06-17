d2ead13b25540811a699b6b9e73345e7c8df71aa
Valentino Constantinou <cconsta1@alumni.nd.edu>
2020-06-18 00:11:46 +0300

    Various fixes

--- viewer_hop.js
+++ viewer_hop.js
@@ -1,5 +1,4 @@
 (function ($, Configuration) {
-
     var useTab = Configuration.tab;
     var referenceUrl = Configuration.url;
     var confId = Configuration.id;
@@ -9,7 +8,7 @@
 
     var scripts = ["corto.js", "nexus.js", "trackball_pantilt.js", "trackball_turntable_pan.js", 
                    "trackball_rail.js", "trackball_sphere.js", "meco.js", "trackball_turntable.js", 
-                   "ply.js", "spidergl.js", "presenter.js", "init.js", "jquery.js"];
+                   "ply.js", "spidergl.js", "presenter.js", "init_custom.js", "jquery.js"];
 
    for (index = 0; index < scripts.length; index++) 
     {
@@ -18,6 +17,8 @@
       s.src = Configuration.previewer +  "/examples/js/" + scripts[index];
       $(useTab).append(s);
     }
+
+   $(useTab).append('<script>var $oldjQuery = $.noConflict(true);</script>')               
 
    $(useTab).append($('<div/>', {
         id: '3dhop',
@@ -33,43 +34,24 @@
        id: 'draw-canvas',
        style: 'background-image: url("/assets/javascripts/previewers/examples/skins/backgrounds/light.jpg")'
     }));
-
-//    function setup3dhop() { 
-//	presenter = new Presenter("draw-canvas");
-
-//	presenter.setScene({
-//		meshes: {
-//			"Gargoyle" : { url: "models/singleres/gargo.ply" }
-//		},
-//		modelInstances : {
-//		"Model1" : { mesh : "Gargoyle" }
-//		}
-//	});
-//}
-
-//$(document).ready(function(){
-//	init3dhop();
-
-//	setup3dhop();
-//});
     
-//    $(useTab).append("<script type='text/javascript'>" +    
-//      "var presenter = null;" +
-//      "function setup3dhop() {" +
-//      "presenter = new Presenter('draw-canvas');" + 
-//      "presenter.setScene({" +
-//		"meshes: {" +
-//			"'Gargoyle' : { url: '/assets/javascripts/previewers/examples/models/singleres/gargo.ply' }" +
-//		"}," +
-//		"modelInstances : {" +
-//			"'Model1' : { mesh : 'Gargoyle' }" +
-//		"}" +
-//	"});" +
-//"};" +
-//"$(document).ready(function($){" +                                                                                      
-//        "init3dhop();" +                                                                                              
-//        "setup3dhop();" +                                                                                             
-//"});" +   
-//"</script>");
+    $(useTab).append("<script type='text/javascript'>" +    
+      "var presenter = null;" +
+      "function setup3dhop() {" +
+      "presenter = new Presenter('draw-canvas');" + 
+      "presenter.setScene({" +
+		"meshes: {" +
+			"'Gargoyle' : { url: '/assets/javascripts/previewers/examples/models/singleres/gargo.ply' }" +
+		"}," +
+		"modelInstances : {" +
+			"'Model1' : { mesh : 'Gargoyle' }" +
+		"}" +
+	"});" +
+"};" +
+"$oldjQuery(document).ready(function($oldjQuery){" +                                                                                      
+       "init3dhop();" +                                                                                              
+       "setup3dhop();" +                                                                                             
+"});" +   
+"</script>");
 
 }(jQuery, Configuration));
