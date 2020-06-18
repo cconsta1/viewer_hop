3d68c0d19bd81ac7270f10bb801aae60ffe027f5
Valentino Constantinou <cconsta1@alumni.nd.edu>
2020-06-18 21:38:57 +0300

    Updating the viewer_hop.js

--- viewer_hop.js
+++ viewer_hop.js
@@ -8,7 +8,7 @@
 
     var scripts = ["corto.js", "nexus.js", "trackball_pantilt.js", "trackball_turntable_pan.js", 
                    "trackball_rail.js", "trackball_sphere.js", "meco.js", "trackball_turntable.js", 
-                   "ply.js", "spidergl.js", "presenter.js", "init_custom.js", "jquery.js"];
+                   "ply.js", "spidergl.js", "presenter.js", "init.js", "jquery.js"];
 
    for (index = 0; index < scripts.length; index++) 
     {
@@ -18,7 +18,11 @@
       $(useTab).append(s);
     }
 
+   $(useTab).append( "1st loaded jQuery version ($): " + $.fn.jquery + "<br>" );
+
    $(useTab).append('<script>var $oldjQuery = $.noConflict(true);</script>')               
+
+   $(useTab).append( "2nd loaded jQuery version ($): " + $oldjQuery.fn.jquery + "<br>" );
 
    $(useTab).append($('<div/>', {
         id: '3dhop',
@@ -34,24 +38,52 @@
        id: 'draw-canvas',
        style: 'background-image: url("/assets/javascripts/previewers/examples/skins/backgrounds/light.jpg")'
     }));
+
+  /*  document.write( '<script type=\"text/javascript\">\n' );
+    document.write( 'var presenter = null;\n' );
+    document.write( '\n' );
+    document.write( 'function setup3dhop() { \n' );
+    document.write( '	presenter = new Presenter(\"draw-canvas\");\n' );
+    document.write( '\n' );
+    document.write( '	presenter.setScene({\n' );
+    document.write( '		meshes: {\n' );
+    document.write( '			\"Gargoyle\" : { url: \"/assets/javascripts/previewers/examples/models/singleres/gargo.ply\" }\n' );
+    document.write( '		},\n' );
+    document.write( '		modelInstances : {\n' );
+    document.write( '			\"Model1\" : { mesh : \"Gargoyle\" }\n' );
+    document.write( '}\n' );
+    document.write( '});\n' );
+    document.write( '}\n' );
+    document.write( '\n' );
+    document.write( '$oldjQuery(document).ready(function(){\n' );
+    document.write( 'init3dhop();\n' );
+    document.write( '\n' );
+    document.write( 'setup3dhop();\n' );
+    document.write( '});\n' );
+    document.write( '</script>' );
+*/
+
     
     $(useTab).append("<script type='text/javascript'>" +    
       "var presenter = null;" +
+
       "function setup3dhop() {" +
       "presenter = new Presenter('draw-canvas');" + 
+
       "presenter.setScene({" +
-		"meshes: {" +
+     		"meshes: {" +
 			"'Gargoyle' : { url: '/assets/javascripts/previewers/examples/models/singleres/gargo.ply' }" +
-		"}," +
-		"modelInstances : {" +
-			"'Model1' : { mesh : 'Gargoyle' }" +
 		"}" +
 	"});" +
-"};" +
-"$oldjQuery(document).ready(function($oldjQuery){" +                                                                                      
-       "init3dhop();" +                                                                                              
-       "setup3dhop();" +                                                                                             
-"});" +   
-"</script>");
+
+       "}" +
+
+       "$oldjQuery(document).ready(function($oldjQuery){" +                                                                                      
+         "init3dhop();" +                                                                                              
+         "setup3dhop();" +                                                                                             
+         "resizeCanvas(600,600);" +
+        "});" +   
+      
+       "</script>");
 
 }(jQuery, Configuration));
