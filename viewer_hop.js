6f1794d9fac1fdf282dda86f10912c14805b424e
Valentino Constantinou <cconsta1@alumni.nd.edu>
2020-06-26 16:38:21 +0300

    Change the examples folder to hop

--- viewer_hop.js
+++ viewer_hop.js
@@ -4,7 +4,7 @@
     var confId = Configuration.id;
     var fileId = Configuration.fileid;
 
-    $(useTab).append('<link rel="stylesheet" type="text/css" href="/assets/javascripts/previewers/examples/stylesheet/3dhop.css">');
+    $(useTab).append('<link rel="stylesheet" type="text/css" href="/assets/javascripts/previewers/hop/stylesheet/3dhop.css">');
 
     var scripts = ["spidergl.js", "presenter.js", "nexus.js", "ply.js", "trackball_sphere.js",
                    "trackball_turntable.js", "trackball_turntable_pan.js", "trackball_pantilt.js", "init.js"];
@@ -13,7 +13,7 @@
     {
       var s = document.createElement("script");
       s.type = "text/javascript";
-      s.src = Configuration.previewer +  "/examples/js/" + scripts[index];
+      s.src = Configuration.previewer +  "/hop/js/" + scripts[index];
       $(useTab).append(s);
     }
 
@@ -29,7 +29,7 @@
 
     $('.tdhop').append($('<canvas/>', {
        id: 'draw-canvas',
-       style: 'background-image: url("/assets/javascripts/previewers/examples/skins/backgrounds/light.jpg")'
+       style: 'background-image: url("/assets/javascripts/previewers/hop/skins/backgrounds/light.jpg")'
     }));
 
     //alert(typeof(referenceUrl));
@@ -44,7 +44,7 @@
 	  'presenter.setScene({ \n' +
 
 		  'meshes: { \n' +
-			  '"mesh_1" : { url: "/assets/javascripts/previewers/examples/models/singleres/wing.ply" } \n' +
+			  '"mesh_1" : { url: "/assets/javascripts/previewers/hop/models/singleres/wing.ply" } \n' +
 		  '}, \n' +
 		  'modelInstances : { \n' +
 			  '"instance_1" : { mesh : "mesh_1" } \n' +
