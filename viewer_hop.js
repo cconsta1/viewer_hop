639c73937640dd6505f04ddccc72fbe6166fdd7e
Valentino Constantinou <cconsta1@alumni.nd.edu>
2020-07-23 15:50:54 +0300

    Fix the URL issue, working version

--- viewer_hop.js
+++ viewer_hop.js
@@ -4,15 +4,23 @@
     var confId = Configuration.id;
     var fileId = Configuration.fileid;
 
-    for (const property in Configuration) 
+    // print all attributes of Configuration object
+
+    /*for (const property in Configuration) 
     {
       alert(`${property}: ${Configuration[property]}`);
-    }
+    }*/
+
+    // add 3dhop.css to previewer
 
     $(useTab).append('<link rel="stylesheet" type="text/css" href="/assets/javascripts/previewers/hop/stylesheet/3dhop.css">');
 
+    // scripts  holds all the  3dhop files
+
     var scripts = ["spidergl.js", "presenter.js", "nexus.js", "ply.js", "trackball_sphere.js",
                    "trackball_turntable.js", "trackball_turntable_pan.js", "trackball_pantilt.js", "init.js"];
+
+    // load 3dhop into the current tab
 
     for (index = 0; index < scripts.length; index++) 
     {
@@ -21,6 +29,8 @@
       s.src = Configuration.previewer +  "/hop/js/" + scripts[index];
       $(useTab).append(s);
     }
+
+   // load various 3dhop attributes (such as the background image) for 3dhop
 
    $(useTab).append($('<div/>', {
         id: '3dhop',
@@ -32,37 +42,58 @@
         id: 'tdhlg'                                                                                                                                      
     })); 
 
-    $('.tdhop').append($('<canvas/>', {
+
+   $('.tdhop').append($('<div/>', {                                                                           
+        id: 'toolbar'                                                                                          
+    }));
+
+   $("#toolbar").append("<img id='home' title='Home'  src='/assets/javascripts/previewers/hop/skins/dark/home.png'/><br/>");
+   $("#toolbar").append("<img id='zoomin' title='Zoom In'  src='/assets/javascripts/previewers/hop/skins/dark/zoomin.png'/><br/>");
+   $("#toolbar").append("<img id='zoomout' title='Zoom Out'  src='/assets/javascripts/previewers/hop/skins/dark/zoomout.png'/><br/>");
+   $("#toolbar").append("<img id='light_on' title='Disable Light Control'  src='/assets/javascripts/previewers/hop/skins/dark/lightcontrol_on.png' style='position:absolute; visibility:hidden;'/>");   
+   $("#toolbar").append("<img id='light' title='Enable Light Control'  src='/assets/javascripts/previewers/hop/skins/dark/lightcontrol.png'/><br/>");
+   $("#toolbar").append("<img id='full_on' title='Exit Full Screen'  src='/assets/javascripts/previewers/hop/skins/dark/full_on.png' style='position:absolute; visibility:hidden;'/>");
+   $("#toolbar").append("<img id='full' title='Full Screen'  src='/assets/javascripts/previewers/hop/skins/dark/full.png'/>"); 
+
+   $('.tdhop').append($('<canvas/>', {
        id: 'draw-canvas',
        style: 'background-image: url("/assets/javascripts/previewers/hop/skins/backgrounds/light.jpg")'
-    }));
- 
-    $(useTab).append('<script> \n' +
-    'var presenter = null; \n \n' +
+   }));
 
-    'function setup3dhop() { \n' +
-	 'presenter = new Presenter("draw-canvas");  \n' +
+   $(document).ready(function(){
+	init3dhop();
 
-	  'presenter.setScene({ \n' +
+	setup3dhop(referenceUrl);
 
-		  'meshes: { \n' +
-			  "\"mesh_1\" : { url: " + "\"" + referenceUrl + "\"" + "} \n" +
-		  '}, \n' +
-		  'modelInstances : { \n' +
-			  '"instance_1" : { mesh : "mesh_1" } \n' +
-		  '} \n' +
-	  '}); \n' +
-    '}; \n' + 
+        resizeCanvas(640,480); 
 
-    'init3dhop(); \n' +
-  
-    'setup3dhop(); \n' +
-
-    'resizeCanvas(640,480); \n' +
-
-    'document.getElementById(\'draw-canvas\').setAttribute(\'width\', document.body.clientWidth); \n' +
-    'document.getElementById(\'draw-canvas\').setAttribute(\'height\', document.body.clientHeight); \n' +
-    '</script>'
-    );
+        moveToolbar(20,20);
+    });
   
 }(jQuery, Configuration));
+
+var presenter = null;
+ 
+function setup3dhop(address) {      
+        presenter = new Presenter("draw-canvas");
+ 
+        presenter.setScene({
+                meshes: {
+                        "mesh_1" : {                             
+                                     url: address, 
+                                     mType: "ply"
+                                   }                      
+                },                                        
+                modelInstances : {                        
+                        "instance_1" : { mesh : "mesh_1" }
+                }
+        });  
+}
+
+function actionsToolbar(action) {
+	if(action=='home') presenter.resetTrackball(); 
+	else if(action=='zoomin') presenter.zoomIn();
+	else if(action=='zoomout') presenter.zoomOut(); 
+	else if(action=='light' || action=='light_on') { presenter.enableLightTrackball(!presenter.isLightTrackballEnabled()); lightSwitch(); } 
+	else if(action=='full'  || action=='full_on') fullscreenSwitch(); 
+}
