ff40cfaf60edc5d412de4ac30255b4acc1cc5fea
Valentino Constantinou <cconsta1@alumni.nd.edu>
2020-09-14 14:26:30 +0300

    Formatting changes

--- viewer_hop.js
+++ viewer_hop.js
@@ -6,13 +6,13 @@
   var previewer = Configuration.previewer;
 
   var fileName = $('#file-name-title').text().trim();
-  var fileNameExtension = fileName.substr(fileName.length - 3); 
+  var fileNameExtension = fileName.substr(fileName.length - 3);
   var fileType;
 
-  if(fileNameExtension == "ply"){
+  if (fileNameExtension == "ply") {
     fileType = "ply";
   }
-  else if (fileNameExtension == "nxz"){
+  else if (fileNameExtension == "nxz") {
     fileType = "nexus";
   }
 
@@ -23,14 +23,13 @@
 
   // print all attributes of Configuration object
 
-  /*for (const property in Configuration) 
-  {
+  /*for (const property in Configuration) {
     alert(`${property}: ${Configuration[property]}`);
   }*/
 
   // add 3dhop.css to previewer
 
-  $(useTab).append('<link rel="stylesheet" type="text/css" href="'+ previewer +'/hop/stylesheet/3dhop.css">');
+  $(useTab).append('<link rel="stylesheet" type="text/css" href="' + previewer + '/hop/stylesheet/3dhop.css">');
 
   // scripts  holds all the  3dhop files
 
@@ -54,28 +53,28 @@
     onmousedown: 'if (event.preventDefault) event.preventDefault()'
   }));
 
-  $("#3dhop").append($('<div/>', {                                                                                                                  
-    id: 'tdhlg'                                                                                                                                     
-  })); 
+  $("#3dhop").append($('<div/>', {
+    id: 'tdhlg'
+  }));
 
   $("#3dhop").append($('<div/>', {
     id: 'toolbar'
   }));
 
-  $("#toolbar").append("<img id='home' title='Home'  src='"+ previewer +"/hop/skins/dark/home.png'/><br/>");
-  $("#toolbar").append("<img id='zoomin' title='Zoom In'  src='"+ previewer +"/hop/skins/dark/zoomin.png'/><br/>");
-  $("#toolbar").append("<img id='zoomout' title='Zoom Out'  src='"+ previewer +"/hop/skins/dark/zoomout.png'/><br/>");
-  $("#toolbar").append("<img id='light_on' title='Disable Light Control'  src='"+ previewer +"/hop/skins/dark/lightcontrol_on.png' style='position:absolute; visibility:hidden;'/>");
-  $("#toolbar").append("<img id='light' title='Enable Light Control'  src='"+ previewer +"/hop/skins/dark/lightcontrol.png'/><br/>");
- 
-  $("#toolbar").append("<img id='measure_on' title='Disable Measure Tool'  src='"+ previewer +"/hop/skins/dark/measure_on.png' style='position:absolute; visibility:hidden;'/>");
-  $("#toolbar").append("<img id='measure' title='Enable Measure Tool'  src='"+ previewer +"/hop/skins/dark/measure.png'/><br/>");
+  $("#toolbar").append("<img id='home' title='Home'  src='" + previewer + "/hop/skins/dark/home.png'/><br/>");
+  $("#toolbar").append("<img id='zoomin' title='Zoom In'  src='" + previewer + "/hop/skins/dark/zoomin.png'/><br/>");
+  $("#toolbar").append("<img id='zoomout' title='Zoom Out'  src='" + previewer + "/hop/skins/dark/zoomout.png'/><br/>");
+  $("#toolbar").append("<img id='light_on' title='Disable Light Control'  src='" + previewer + "/hop/skins/dark/lightcontrol_on.png' style='position:absolute; visibility:hidden;'/>");
+  $("#toolbar").append("<img id='light' title='Enable Light Control'  src='" + previewer + "/hop/skins/dark/lightcontrol.png'/><br/>");
 
-  $("#toolbar").append("<img id='pick_on' title='Disable PickPoint Mode'  src='"+ previewer +"/hop/skins/dark/pick_on.png' style='position:absolute; visibility:hidden;'/>");
-  $("#toolbar").append("<img id='pick' title='Enable PickPoint Mode'  src='"+ previewer +"/hop/skins/dark/pick.png'/><br/>");                                                                                              
+  $("#toolbar").append("<img id='measure_on' title='Disable Measure Tool'  src='" + previewer + "/hop/skins/dark/measure_on.png' style='position:absolute; visibility:hidden;'/>");
+  $("#toolbar").append("<img id='measure' title='Enable Measure Tool'  src='" + previewer + "/hop/skins/dark/measure.png'/><br/>");
 
-  $("#toolbar").append("<img id='full_on' title='Exit Full Screen'  src='"+ previewer +"/hop/skins/dark/full_on.png' style='position:absolute; visibility:hidden;'/>");
-  $("#toolbar").append("<img id='full' title='Full Screen'  src='"+ previewer +"/hop/skins/dark/full.png'/>");
+  $("#toolbar").append("<img id='pick_on' title='Disable PickPoint Mode'  src='" + previewer + "/hop/skins/dark/pick_on.png' style='position:absolute; visibility:hidden;'/>");
+  $("#toolbar").append("<img id='pick' title='Enable PickPoint Mode'  src='" + previewer + "/hop/skins/dark/pick.png'/><br/>");
+
+  $("#toolbar").append("<img id='full_on' title='Exit Full Screen'  src='" + previewer + "/hop/skins/dark/full_on.png' style='position:absolute; visibility:hidden;'/>");
+  $("#toolbar").append("<img id='full' title='Full Screen'  src='" + previewer + "/hop/skins/dark/full.png'/>");
 
   $('#3dhop').append($('<div/>', {
     id: 'measure-box',
@@ -84,36 +83,36 @@
 
   $("#measure-box").text("Measured length");
 
-  $("#measure-box").append($('<hr/>')); 
+  $("#measure-box").append($('<hr/>'));
 
   $("#measure-box").append($('<span/>', {
-      id: 'measure-output',
-      class : 'output-text',
-      onmousedown: 'event.stopPropagation()'
-   }));
-  
+    id: 'measure-output',
+    class: 'output-text',
+    onmousedown: 'event.stopPropagation()'
+  }));
+
   $("measure-output").text("0.0");
 
-  $('#3dhop').append($('<div/>', {                                                                                                          
-    id: 'pickpoint-box',                                                                                                                      
-    class: 'output-box'                                                                                                                     
-  }));                                                                                                                                      
-                                                                                                                                            
-  $("#pickpoint-box").text("XYZ picked point");                                                                                                
-                                                                                                                                            
-  $("#pickpoint-box").append($('<hr/>'));                                                                                                     
-                                                                                                                                            
-  $("#pickpoint-box").append($('<span/>', {                                                                                                   
-      id: 'pickpoint-output',                                                                                                                 
-      class : 'output-text',                                                                                                                
-      onmousedown: 'event.stopPropagation()'                                                                                                
-   }));                                                                                                                                     
-                                                                                                                                            
-  $("pickpoint-output").text("[ 0 , 0 , 0 ]");   
+  $('#3dhop').append($('<div/>', {
+    id: 'pickpoint-box',
+    class: 'output-box'
+  }));
+
+  $("#pickpoint-box").text("XYZ picked point");
+
+  $("#pickpoint-box").append($('<hr/>'));
+
+  $("#pickpoint-box").append($('<span/>', {
+    id: 'pickpoint-output',
+    class: 'output-text',
+    onmousedown: 'event.stopPropagation()'
+  }));
+
+  $("pickpoint-output").text("[ 0 , 0 , 0 ]");
 
   $("#3dhop").append($('<canvas/>', {
     id: 'draw-canvas',
-    style: 'background-image: url("'+ previewer +'/hop/skins/backgrounds/black.jpg")'
+    style: 'background-image: url("' + previewer + '/hop/skins/backgrounds/black.jpg")'
   }));
 
   $(document).ready(function () {
@@ -123,8 +122,8 @@
 
     resizeCanvas(640, 480);
 
-    moveMeasurementbox(70,243); 
-    movePickpointbox(70,301);  
+    moveMeasurementbox(70, 243);
+    movePickpointbox(70, 301);
 
     //moveToolbar(20, 20);
   });
@@ -147,22 +146,22 @@ function setup3dhop(address, fileType) {
       "instance_1": { mesh: "mesh_1" }
     },
     trackball: {
-	type : TurntablePanTrackball,
-	trackOptions : {
-		startDistance: 1.3,
-		startPhi: 40.0,
-		startTheta: 20.0,
-		minMaxDist: [0.8, 2.5],
-		minMaxPhi: [-180, 180],
-		minMaxTheta: [-30.0, 70.0]
-	}
-	},
-	space: {
-		centerMode: "scene",
-		radiusMode: "scene"
-	}
+      type: TurntablePanTrackball,
+      trackOptions: {
+        startDistance: 1.3,
+        startPhi: 40.0,
+        startTheta: 20.0,
+        minMaxDist: [0.8, 2.5],
+        minMaxPhi: [-180, 180],
+        minMaxTheta: [-30.0, 70.0]
+      }
+    },
+    space: {
+      centerMode: "scene",
+      radiusMode: "scene"
+    }
   });
- 
+
   presenter._onEndMeasurement = onEndMeasure;
   presenter._onEndPickingPoint = onEndPick;
 }
@@ -174,8 +173,9 @@ function actionsToolbar(action) {
   else if (action == 'light' || action == 'light_on') { presenter.enableLightTrackball(!presenter.isLightTrackballEnabled()); lightSwitch(); }
   else if (action == 'measure' || action == 'measure_on') { presenter.enableMeasurementTool(!presenter.isMeasurementToolEnabled()); measureSwitch(); }
   else if (action == 'full' || action == 'full_on') fullscreenSwitch();
-  else if(action=='pick' || action=='pick_on') { 
-    presenter.enablePickpointMode(!presenter.isPickpointModeEnabled()); pickpointSwitch(); } 
+  else if (action == 'pick' || action == 'pick_on') {
+    presenter.enablePickpointMode(!presenter.isPickpointModeEnabled()); pickpointSwitch();
+  }
 }
 
 function onEndMeasure(measure) {
@@ -185,9 +185,8 @@ function onEndMeasure(measure) {
 }
 
 function onEndPick(point) {
-    var x = point[0].toFixed(2);
-    var y = point[1].toFixed(2);
-    var z = point[2].toFixed(2);
-    $('#pickpoint-output').html("[ "+x+" , "+y+" , "+z+" ]");
+  var x = point[0].toFixed(2);
+  var y = point[1].toFixed(2);
+  var z = point[2].toFixed(2);
+  $('#pickpoint-output').html("[ " + x + " , " + y + " , " + z + " ]");
 }
-
